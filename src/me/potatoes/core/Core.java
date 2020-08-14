a141886ec9170ee2b411226c66b0b4b820d25a7c
GilBenDavid <giltalgil987@gmail.com>
2020-08-14 21:24:16 +0300

    Start to work on brewing stand

--- src/me/potatoes/core/Core.java
+++ src/me/potatoes/core/Core.java
@@ -1,7 +1,16 @@
 package me.potatoes.core;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
+
+    public static Core plugin;
+
+    public Core() {
+        this.plugin = this;
+    }
+
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new StandListener(), this);
--- src/me/potatoes/core/StandListener.java
+++ src/me/potatoes/core/StandListener.java
@@ -1,10 +1,33 @@
 package me.potatoes.core;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCreativeEvent;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+
 
 public class StandListener implements Listener {
 
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void potionItemPlacer(final InventoryClickEvent e) {
+        if (e.getClickedInventory() != null &&
+                e.getClickedInventory().getType() == InventoryType.BREWING &&
+                e.getClick() == ClickType.LEFT) {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(Core.plugin, () -> {
+
+                final ItemStack currentItem = e.getCurrentItem();
+                final ItemStack heldItem = e.getCursor().clone();
+                e.setCursor(currentItem);
+                e.getClickedInventory().setItem(e.getSlot(), heldItem);
+                ((Player) e.getView().getPlayer()).updateInventory();
+            }, 1L);
+        }
+    }
 
 }
